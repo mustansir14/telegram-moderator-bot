7eddbfcc3d001b8c3c1074de1434f3392daa1394
Mustansir Muzaffar <mustansir2001@gmail.com>
2023-09-02 16:03:30 +0500

    fix

--- bot.py
+++ bot.py
@@ -55,10 +55,10 @@ def main():
     app.add_handler(MessageHandler(filters.TEXT & ~
                                    filters.COMMAND, delete_negative_messages))
 
-    app.run_polling()
+    # app.run_polling()
 
-    # app.run_webhook(
-    #     "0.0.0.0", PORT, TELEGRAM_BOT_TOKEN, webhook_url="https://telegram-moderator-bot-ace7cd090436.herokuapp.com/" + TELEGRAM_BOT_TOKEN)
+    app.run_webhook(
+        "0.0.0.0", PORT, TELEGRAM_BOT_TOKEN, webhook_url="https://telegram-moderator-bot-ace7cd090436.herokuapp.com/" + TELEGRAM_BOT_TOKEN)
 
 
 if __name__ == '__main__':
